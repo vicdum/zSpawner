886ed4b9cf7865ee364349c4b442ebd256460eb9
Maxlego08 <maxlego08@sfr.fr>
2024-03-08 10:33:52 +0100

    :memo: Fixs

--- src/fr/maxlego08/spawner/ZSpawner.java
+++ src/fr/maxlego08/spawner/ZSpawner.java
@@ -65,7 +65,7 @@ public class ZSpawner extends ZUtils implements Spawner {
     }
 
     public ZSpawner(SpawnerPlugin plugin, UUID spawnerId, UUID ownerId, SpawnerType spawnerType, EntityType entityType, BlockFace blockFace) {
-        this(plugin, spawnerId, ownerId, spawnerType, entityType, 0, null, 0, blockFace);
+        this(plugin, spawnerId, ownerId, spawnerType, entityType, 0, null, 1, blockFace);
     }
 
     public ZSpawner(SpawnerPlugin plugin, UUID ownerId, SpawnerType spawnerType, EntityType entityType, BlockFace blockFace) {
@@ -393,10 +393,11 @@ public class ZSpawner extends ZUtils implements Spawner {
 
         if (System.currentTimeMillis() > this.lastSpawnAt && this.amount < spawnerOption.getMaxEntity()) {
 
-            long ms = ThreadLocalRandom.current().nextLong(spawnerOption.getMinDelay(), spawnerOption.getMaxDelay());
+            long ms = ThreadLocalRandom.current().nextLong(Math.min(spawnerOption.getMinDelay(), spawnerOption.getMaxDelay()), Math.max(spawnerOption.getMinDelay(), spawnerOption.getMaxDelay()));
             this.lastSpawnAt = System.currentTimeMillis() + ms;
 
-            this.amount += getNumberBetween(spawnerOption.getMinSpawn(), spawnerOption.getMaxSpawn());
+            this.amount += getNumberBetween(Math.min(spawnerOption.getMinSpawn(), spawnerOption.getMaxDelay()), Math.max(spawnerOption.getMinSpawn(), spawnerOption.getMaxDelay()));
+            if (this.amount > spawnerOption.getMaxEntity()) this.amount = spawnerOption.getMaxEntity();
             this.needUpdate = true;
             this.updateEntity();
         }
